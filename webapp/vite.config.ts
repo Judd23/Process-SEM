2e08463f50c3c58c53e01eb5d40981805e9686e5
Judd23 <judd23@gmail.com>
2026-01-10 22:04:56 -0800

    config vite

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-// Use '/' for custom domain, '/Dissertation-Model-Simulation/' for GitHub Pages subdomain
 export default defineConfig({
   plugins: [react()],
   base: '/Dissertation-Model-Simulation/',
-})
+});
